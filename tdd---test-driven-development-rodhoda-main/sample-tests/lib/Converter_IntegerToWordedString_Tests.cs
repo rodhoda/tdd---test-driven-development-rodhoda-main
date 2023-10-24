4329147e25f95a256f3d5ba70beb4b2a7651c92a
rodhoda <rod.hoda@gmail.com>
2023-10-24 15:47:55 -0700

    Adding Double Digit Integration Program can now support values from 0 to 99. Code is broked down into three segments, one for singles, another teens, and another for double digits. In the third category, we utilized recurssion to pass the number back after the tens' place is discover, so the new value without the tens digit, can be found and then it's returned in a concatenated string.

--- tdd---test-driven-development-rodhoda-main/sample-tests/lib/Converter_IntegerToWordedString_Tests.cs
+++ tdd---test-driven-development-rodhoda-main/sample-tests/lib/Converter_IntegerToWordedString_Tests.cs
@@ -12,30 +12,66 @@ public class Convert_IntegerToWordedString_Tests
     //     Assert.Throws<NotImplementedException>(act);
     // }
 
+//  Single Digit Unit Testing
     [Fact]
-    //TODO: Initial "red" test to code logic
-    public void SinglesDigitFirstTest()
-    {
+    public void SinglesDigitFirstTest() {
         int number = 1;
         string words = Converter.IntegerToWordedString(number);
         Assert.Equal("one",words,true);
     }
-
     [Fact]
-    public void SinglesDigitSecondTest()
-    {
+    public void SinglesDigitSecondEdgeTest() {
         int number = 9;
         string words = Converter.IntegerToWordedString(number);
         Assert.Equal("nine",words,true);
     }
-
-
     [Fact]
-    public void SinglesDigitThirdTest()
-    {
+    public void SinglesDigitThirdEdgeTest() {
         int number = 0;
         string words = Converter.IntegerToWordedString(number);
         Assert.Equal("zero",words,true);
     }
+
+//  Teens Digit Unit Testing
+    [Fact]
+    public void TeensDigitFirstEdgeTest()   {
+        int number = 19;
+        string words = Converter.IntegerToWordedString(number);
+        Assert.Equal("nineteen", words, true);
+    }
+    [Fact]
+    public void TeensDigitSecondEdgeTest()   {
+        int number = 10;
+        string words = Converter.IntegerToWordedString(number);
+        Assert.Equal("ten", words, true);
+    }
+    [Fact]
+    public void TeensDigitThirdTest()   {
+        int number = 11;
+        string words = Converter.IntegerToWordedString(number);
+        Assert.Equal("eleven", words, true);
+    }
+
+
+    // Double Digit Unit Testing
+    [Fact]
+    public void DoubleDigitFirstTest()   {
+        int number = 24;
+        string words = Converter.IntegerToWordedString(number);
+        Assert.Equal("twenty-four", words, true);
+    }
+    [Fact]
+    public void DoubleDigitSecondTest()   {
+        int number = 99;
+        string words = Converter.IntegerToWordedString(number);
+        Assert.Equal("ninety-nine", words, true);
+    }
+    [Fact]
+    public void DoubleDigitThirdTest()   {
+        int number = 70;
+        string words = Converter.IntegerToWordedString(number);
+        Assert.Equal("seventy-zero", words, true);
+    }
+
     
 }
